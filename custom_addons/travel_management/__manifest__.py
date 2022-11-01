6b5e5b8d509eb202ef4913176d88e0153d817721
WhiteWolf2000 <razidota@gmail.com>
2022-11-01 11:28:13 +0500

    first commit

---
+++ custom_addons/travel_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Travel management',
+    'version': '1.1',
+<<<<<<< HEAD
+    'author': 'Razi',
+    'category': 'Extra tools',
+    'depends': ['base'],
+    'data': [
+        "views/travel_view.xml",
+        "security/ir.model.access.csv"
+    ]
+=======
+    'category': 'Extra tools',
+>>>>>>> 4c74d74b71788ffcc3e3854ac43ee6ac8b05a57c
+
+}
+
